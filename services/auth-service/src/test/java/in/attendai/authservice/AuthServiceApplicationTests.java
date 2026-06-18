78dae9cf8cb7fd8b8d7cc62fef4975e90d0bb288
omrakibe <rakibeom30@gmail.com>
2026-06-18 11:15:32 +0530

    Initiallized auth service to gitHub with all project setup including all required dependencies

---
+++ services/auth-service/src/test/java/in/attendai/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package in.attendai.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
